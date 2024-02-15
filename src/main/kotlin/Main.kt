cc5aa0514c58f90b335ef3f32537f510bbcebcac
Salakhov.R.R <robert_salakhov@bk.ru>
2024-02-15 22:24:15 +0300

    ex4

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,39 +1,60 @@
 fun main() {
-    val dataAuto = arrayOf(
-        "Только сегодня! Спорткар за 9990 кредитов! Спешите купить!"
-    )
+    val dataAuto = getDataAuto()
 
-    val dataMovie = arrayOf(
-        "Новая часть культовой саги \"Звездные волны XIX\". Только в IMax5D!"
-    )
+    val dataMovie = getDataMovie()
 
-    val dataDroid = arrayOf(
-        "Заметил, что дроид уже не так эффективно работает? Чип I.A.M.D.N. уже в магазинах! Обнови своего дроида!"
-    )
+    val dataDroid = getDataDroid()
 
-    val category = readln()
-    var countOfRepeat = readln().toInt()
+    val adType = readln()
+    val steps = readln().toInt()
+    var currentStep = 0
+    do {
+        currentStep++
 
-    while (countOfRepeat > 0) {
-        when (category) {
+        when (adType) {
             "Авто" -> {
-                for (i in dataAuto) {
-                    println(i)
-                }
+                showBillBoard(dataAuto)
             }
 
             "Кино" -> {
-                for (i in dataMovie) {
-                    println(i)
-                }
+                showBillBoard(dataMovie)
             }
 
             "Дроиды" -> {
-                for (i in dataDroid) {
-                    println(i)
-                }
+                showBillBoard(dataDroid)
             }
         }
-        countOfRepeat--
+
+        println()
+    } while (currentStep < steps)
+}
+
+fun getDataAuto(): Array<String> {
+    return arrayOf(
+        "Только сегодня! Спорткар за 9990 кредитов! Спешите купить!",
+        "Встречайте первый Галактический спорткар с открытым верхом. Ограниченная серия уже в нашем магазине!",
+        "Опаздываешь на важные встречи? Покупай спорткар с квантовым двигателем всего за 29990 кредитов!"
+    )
+}
+
+fun getDataMovie(): Array<String> {
+    return arrayOf(
+        "Новая часть культовой саги \"Звездные волны XIX\". Только в IMax5D!",
+        "Что победит - деньги или любовь? Продолжение легендарного фильма \"Летучая мышь в черном\" уже в кинотеатрах!",
+        "Ежегодная часть \"Елочки-Иголочки\" уже в кино! Скорее в кинотеатры!"
+    )
+}
+
+fun getDataDroid(): Array<String> {
+    return arrayOf(
+        "Заметил, что дроид уже не так эффективно работает? Чип IAMD уже в магазинах! Обнови своего дроида!",
+        "Мощнее в 1,5 раза! Дороже в 2 раза! Приобретай нашего дроида!",
+        "Любишь дорогих роботов? Прошлые поколения уже не радуют? Только для тебя! Золотой дроид с инкрустацией драгоценными камнями за 20000 кредитов!"
+    )
+}
+
+fun showBillBoard(data: Array<String>) {
+    for (i in data) {
+        println(i)
     }
 }
